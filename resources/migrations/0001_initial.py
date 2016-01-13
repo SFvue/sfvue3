436164f9f325ce821d8f3c1143c63d99c8b9bf96
bogotobogo <k@bogotobogo.com>
2016-01-12 21:40:15 -0800

    added thumbnail field to resource & upload_to resources/topics/Y/M/D

--- resources/migrations/0001_initial.py
+++ resources/migrations/0001_initial.py
@@ -1,146 +1,93 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Topic'
-        db.create_table('resources_topic', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=60)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255)),
-            ('help_text', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-        ))
-        db.send_create_signal('resources', ['Topic'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-        # Adding model 'ResourceType'
-        db.create_table('resources_resourcetype', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=60)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=255)),
-            ('help_text', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('color', self.gf('django.db.models.fields.CharField')(default='purple', unique=True, max_length=20)),
-        ))
-        db.send_create_signal('resources', ['ResourceType'])
-
-        # Adding model 'Resource'
-        db.create_table('resources_resource', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('title', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(default='', max_length=255, blank=True)),
-            ('url', self.gf('django.db.models.fields.URLField')(unique=True, max_length=200)),
-            ('help_text', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('description', self.gf('django.db.models.fields.TextField')(default='', null=True, blank=True)),
-            ('resource_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['resources.ResourceType'])),
-            ('level', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('created_by', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
-            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-            ('show', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('rating_votes', self.gf('django.db.models.fields.PositiveIntegerField')(default=0, blank=True)),
-            ('rating_score', self.gf('django.db.models.fields.IntegerField')(default=0, blank=True)),
-        ))
-        db.send_create_signal('resources', ['Resource'])
-
-        # Adding M2M table for field topics on 'Resource'
-        db.create_table('resources_resource_topics', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('resource', models.ForeignKey(orm['resources.resource'], null=False)),
-            ('topic', models.ForeignKey(orm['resources.topic'], null=False))
-        ))
-        db.create_unique('resources_resource_topics', ['resource_id', 'topic_id'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Topic'
-        db.delete_table('resources_topic')
-
-        # Deleting model 'ResourceType'
-        db.delete_table('resources_resourcetype')
-
-        # Deleting model 'Resource'
-        db.delete_table('resources_resource')
-
-        # Removing M2M table for field topics on 'Resource'
-        db.delete_table('resources_resource_topics')
-
-
-    models = {
-        'auth.group': {
-            'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
-        },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
-            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
-        },
-        'auth.user': {
-            'Meta': {'object_name': 'User'},
-            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
-            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
-        },
-        'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'resources.resource': {
-            'Meta': {'object_name': 'Resource'},
-            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
-            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
-            'help_text': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'level': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
-            'rating_score': ('django.db.models.fields.IntegerField', [], {'default': '0', 'blank': 'True'}),
-            'rating_votes': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0', 'blank': 'True'}),
-            'resource_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['resources.ResourceType']"}),
-            'show': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'default': "''", 'max_length': '255', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'topics': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['resources.Topic']", 'symmetrical': 'False'}),
-            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'url': ('django.db.models.fields.URLField', [], {'unique': 'True', 'max_length': '200'})
-        },
-        'resources.resourcetype': {
-            'Meta': {'object_name': 'ResourceType'},
-            'color': ('django.db.models.fields.CharField', [], {'default': "'purple'", 'unique': 'True', 'max_length': '20'}),
-            'help_text': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '60'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'})
-        },
-        'resources.topic': {
-            'Meta': {'object_name': 'Topic'},
-            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'help_text': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '60'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '255'})
-        }
-    }
-
-    complete_apps = ['resources']
+    operations = [
+        migrations.CreateModel(
+            name='FeaturedResource',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Resource',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=255)),
+                ('slug', models.SlugField(default=b'', max_length=255, blank=True)),
+                ('url', models.URLField(unique=True)),
+                ('help_text', models.CharField(max_length=255, null=True, blank=True)),
+                ('description', models.TextField(default=b'', null=True, blank=True)),
+                ('level', models.CharField(default=b'introductory', max_length=30, verbose_name=b'Level of depth', blank=True, choices=[(b'introductory', b'introductory'), (b'intermediate', b'intermediate'), (b'in-depth', b'in-depth')])),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('show', models.BooleanField(default=True)),
+                ('rating_votes', models.PositiveIntegerField(default=0, editable=False, blank=True)),
+                ('rating_score', models.IntegerField(default=0, editable=False, blank=True)),
+                ('created_by', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ResourceType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=60)),
+                ('slug', models.SlugField(max_length=255)),
+                ('help_text', models.CharField(max_length=255, null=True, blank=True)),
+                ('color', models.CharField(default=b'purple', unique=True, max_length=20)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Topic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=60)),
+                ('slug', models.SlugField(max_length=255)),
+                ('help_text', models.CharField(max_length=255, null=True, blank=True)),
+                ('description', models.TextField(null=True, blank=True)),
+                ('thumbnail', models.ImageField(null=True, upload_to=b'topics/%Y/%m/%d/', blank=True)),
+                ('official_website', models.URLField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['name'],
+            },
+        ),
+        migrations.AddField(
+            model_name='resource',
+            name='resource_type',
+            field=models.ForeignKey(default=2, to='resources.ResourceType'),
+        ),
+        migrations.AddField(
+            model_name='resource',
+            name='topics',
+            field=models.ManyToManyField(to='resources.Topic'),
+        ),
+        migrations.AddField(
+            model_name='featuredresource',
+            name='resource',
+            field=models.ForeignKey(to='resources.Resource'),
+        ),
+        migrations.AddField(
+            model_name='featuredresource',
+            name='resource_type',
+            field=models.ForeignKey(to='resources.ResourceType'),
+        ),
+        migrations.AddField(
+            model_name='featuredresource',
+            name='topic',
+            field=models.ForeignKey(to='resources.Topic'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='featuredresource',
+            unique_together=set([('topic', 'resource_type')]),
+        ),
+    ]
